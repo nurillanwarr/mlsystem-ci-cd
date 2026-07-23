2e4c7cb142cab2c78ccaec06832c9da2365ba7ea
Nuril Anwar <spensaskh1@gmail.com>
2026-07-24 03:11:22 +0700

    Remove hardcoded tracking URI

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,7 +6,7 @@ import mlflow
 import mlflow.sklearn
 
 if __name__ == "__main__":
-    mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+
     mlflow.set_experiment("Latihan Credit Scoring")
     
     with mlflow.start_run():
