77d2720574f37934139380f5af2ee2b841f43d0a
Nuril Anwar <spensaskh1@gmail.com>
2026-07-26 03:35:33 +0700

    Set MLFLOW_ALLOW_FILE_STORE explicitly in python script to fix MLflow error

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,16 +6,20 @@ from sklearn.metrics import accuracy_score
 import mlflow
 import mlflow.sklearn
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    import os
 
+    os.environ["MLFLOW_ALLOW_FILE_STORE"] = "true"
     with mlflow.start_run():
         data = load_breast_cancer()
         df = pd.DataFrame(data.data, columns=data.feature_names)
-        df['target'] = data.target
+        df["target"] = data.target
 
-        X = df.drop('target', axis=1)
-        y = df['target']
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        X = df.drop("target", axis=1)
+        y = df["target"]
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=0.2, random_state=42
+        )
 
         model = RandomForestClassifier(n_estimators=10, random_state=42)
         model.fit(X_train, y_train)
@@ -23,6 +27,6 @@ if __name__ == '__main__':
         preds = model.predict(X_test)
         acc = accuracy_score(y_test, preds)
 
-        mlflow.log_metric('accuracy', acc)
-        mlflow.sklearn.log_model(model, 'model')
-        print(f'Model trained. Accuracy: {acc}')
+        mlflow.log_metric("accuracy", acc)
+        mlflow.sklearn.log_model(model, "model")
+        print(f"Model trained. Accuracy: {acc}")
